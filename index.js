8bb111dea4775798054034f37dff590d1abe6841
PranavAnand587 <pranav20172017@gmail.com>
2022-11-17 07:04:39 +0530

    Positioning of Sharks in the Ocean Container

--- index.js
+++ index.js
@@ -15,3 +15,69 @@ $(document).scroll(() => {
   }
   $(".depth-line span").text(scrollTop)
 })
+
+
+const Data = [{
+  "range":50,
+  "Image":"./assets/Dusky-Shark-01.svg",
+},
+{
+  "range":400,
+  "Image":"./assets/Dwarf-Gulper-Shark-01.svg",
+},
+{
+  "range":1200,
+  "Image":"./assets/Black-Spot-Shark-01.svg",
+},
+{
+  "range":850,
+  "Image":"./assets/Blue-Shark-01.svg",
+},
+{
+  "range":1800,
+  "Image":"./assets/Arrowhead-Dogfish-Shark-01.svg",
+},
+{
+  "range":1700,
+  "Image":"./assets/Arabian-Smooth-Hound-Shark-01.svg",
+},
+];
+
+
+
+function displayShark(sharkData){
+  for (let i = 0; i < sharkData.length; i++) {
+    const shark = sharkData[i];
+    const sharkRange = shark["range"];
+
+    const rand_one = Math.random();
+    const align = (rand_one>=0.5) ? "start":"end";
+
+    const sharkDiv = document.createElement("div");
+    const sharkDivCol = document.createElement("div");
+    const sharkImg = document.createElement("img");
+    
+    sharkDiv.id = i;
+    sharkDiv.classList.add("row","justify-content-"+align);
+    sharkDiv.setAttribute("style","top:"+sharkRange+"px")
+    sharkDivCol.classList.add("col-6");
+    sharkImg.classList.add("img")
+    sharkImg.setAttribute("src",shark["Image"]);
+    
+    sharkDivCol.appendChild(sharkImg);
+    sharkDiv.appendChild(sharkDivCol);
+    
+    if(sharkRange>=200 && sharkRange<1000){
+      document.getElementById("twilight-zone").appendChild(sharkDiv);
+    }else if(sharkRange>=1000){
+      document.getElementById("midnight-zone").appendChild(sharkDiv);
+    }else{
+      document.getElementById("sunlight-zone").appendChild(sharkDiv);
+    }
+
+
+  }
+  
+};
+
+displayShark(Data);
